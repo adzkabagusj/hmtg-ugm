a785f346ff36617dd3b8f2a9081ac40689d36e64
hilmiwismadi <dzakiwismadi@gmail.com>
2026-04-21 22:40:01 +0700

    feat: update cms hostname

--- site/next.config.ts
+++ site/next.config.ts
@@ -13,38 +13,15 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
+        protocol: "https",
+        hostname: "cms-hmtg.webbuild.arachnova.id",
+        pathname: "/uploads/**",
+      },
+      {
         protocol: "http",
         hostname: "localhost",
         port: "1337",
         pathname: "/uploads/**",
-      },
-      {
-        protocol: "http",
-        hostname: "127.0.0.1",
-        port: "1337",
-        pathname: "/uploads/**",
-      },
-      {
-        protocol: "https",
-        hostname: "certain-angel-bc161b7e61.strapiapp.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "certain-angel-bc161b7e61.strapiapp.comhttps",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "certain-angel-bc161b7e61.media.strapiapp.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "*.strapi.app", // Untuk Strapi Cloud
       },
     ],
   },
